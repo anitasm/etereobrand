e429b7317de4950ad8bc37f992d5dea57d2c95f7
anasolmendoza <soy@unmail.real>
2025-12-30 08:29:06 -0300

    Links creados con url a imágenes y firebase vinculado

--- src/data/products.js
+++ src/data/products.js
@@ -1,3 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDn7I3J0y_alvjIXvez2f7fnlmRbKd_ceQ",
+  authDomain: "etereobrand-ce927.firebaseapp.com",
+  projectId: "etereobrand-ce927",
+  storageBucket: "etereobrand-ce927.firebasestorage.app",
+  messagingSenderId: "1000084488702",
+  appId: "1:1000084488702:web:0aaf6e1d969b63dad900af"
+};
+
+export const app = initializeApp(firebaseConfig);
+
 export const products = [
   {
     id: 'buzo-arena',
@@ -72,3 +85,5 @@ export const products = [
     featured: true,
   },
 ]
+
+
